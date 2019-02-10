808a1cd4b7d809f983b1b1876d261fa38f7d4e32
Yafan Liu <12176172+YafanLiu@users.noreply.github.com>
2019-02-09 19:50:37 -0500

    update ece1779a1-1.sql

--- A1/ece1779a1-1.sql
+++ A1/ece1779a1-1.sql
@@ -16,7 +16,6 @@ DROP SCHEMA IF EXISTS `ece1779a1` ;
 -- Schema ece1779a1
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `ece1779a1` DEFAULT CHARACTER SET utf8 ;
-SHOW WARNINGS;
 USE `ece1779a1` ;
 
 -- -----------------------------------------------------
@@ -24,35 +23,30 @@ USE `ece1779a1` ;
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `ece1779a1`.`images` ;
 
-SHOW WARNINGS;
 CREATE TABLE IF NOT EXISTS `ece1779a1`.`images` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `filepath` VARCHAR(200) NOT NULL,
   `description` VARCHAR(100) NULL,
   PRIMARY KEY (`id`));
 
-SHOW WARNINGS;
 
 -- -----------------------------------------------------
 -- Table `ece1779a1`.`user`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `ece1779a1`.`user` ;
 
-SHOW WARNINGS;
 CREATE TABLE IF NOT EXISTS `ece1779a1`.`user` (
   `username` VARCHAR(20) NOT NULL,
   `password` VARCHAR(32) NOT NULL,
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`));
 
-SHOW WARNINGS;
 
 -- -----------------------------------------------------
 -- Table `ece1779a1`.`user_has_images`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `ece1779a1`.`user_has_images` ;
 
-SHOW WARNINGS;
 CREATE TABLE IF NOT EXISTS `ece1779a1`.`user_has_images` (
   `user_id` INT NOT NULL,
   `images_id` INT NOT NULL,
@@ -67,10 +61,8 @@ CREATE TABLE IF NOT EXISTS `ece1779a1`.`user_has_images` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
-SHOW WARNINGS;
 CREATE INDEX `fk_user_has_images_user_idx` ON `ece1779a1`.`user_has_images` (`user_id` ASC);
 
-SHOW WARNINGS;
 CREATE INDEX `fk_user_has_images_images1_idx` ON `ece1779a1`.`user_has_images` (`images_id` ASC);
 
 CREATE USER 'ece1779a1' IDENTIFIED BY 'secret';
